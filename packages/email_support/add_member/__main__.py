3ee046d53cf7f1a0404c2272770b582f92b58f95
houfu <houfu@outlook.stg>
2023-03-26 10:59:49 +0800

    Add: Welcome email

--- packages/email_support/add_member/__main__.py
+++ packages/email_support/add_member/__main__.py
@@ -14,6 +14,17 @@ def main(args):
             'address': new_member
         }
     )
+    if r.status_code == 200:
+        requests.post(
+            "https://api.mailgun.net/v3/mg.your-amicus.app/messages",
+            auth=("api", key),
+            data={
+                "from": "SG Law Cookies by Your Amicus <cookies@your-amicus.app>",
+                "to": new_member,
+                "subject": "Thanks for subscribing to SG Law Cookies",
+                "template": "welcome_sg_cookies"
+            }
+        )
     return {
         "body": {
             "message": r.json()['message'],
