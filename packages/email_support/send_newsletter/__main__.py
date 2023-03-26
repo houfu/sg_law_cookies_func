95a5e0eb2b1fcd0433fb0dcbf09048682cb01008
houfu <houfu@outlook.stg>
2023-03-27 00:42:47 +0800

    Add: Send Newsletter

---
+++ packages/email_support/send_newsletter/__main__.py
@@ -0,0 +1,27 @@
+import os
+
+import requests
+
+address = "sg_law_cookies@mg.your-amicus.app"
+
+
+def main(args):
+    key = os.getenv('MAILGUN_API_KEY')
+    title = args.get('title')
+    content = args.get('content')
+    r = requests.post(
+        "https://api.mailgun.net/v3/sg_law_cookies@mg.your-amicus.app/messages",
+        auth=("api", key),
+        data={
+            "from": "SG Law Cookies by Your Amicus <cookies@your-amicus.app>",
+            "to": address,
+            "subject": title,
+            "html": content
+        }
+    )
+    return {
+        "body": {
+            "message": r.json()['message'],
+            "status_code": r.status_code
+        }
+    }
