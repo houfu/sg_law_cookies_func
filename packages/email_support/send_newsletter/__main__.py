9ca73e1149ca1c89dfb5ec8106c0bf52c9973f06
houfu <houfu@outlook.stg>
2023-03-27 23:33:49 +0800

    Fix: Send Newsletter: Fix typo

--- packages/email_support/send_newsletter/__main__.py
+++ packages/email_support/send_newsletter/__main__.py
@@ -10,16 +10,13 @@ def main(args):
     title = args.get('title')
     content = args.get('content')
     r = requests.post(
-        "https://api.mailgun.net/v3/sg_law_cookies@mg.your-amicus.app/messages",
+        "https://api.mailgun.net/v3/mg.your-amicus.app/messages",
         auth=("api", key),
         data={
             "from": "SG Law Cookies by Your Amicus <cookies@your-amicus.app>",
             "to": address,
             "subject": title,
             "html": content
-        },
-        headers={
-            "Content-Type": "application/json"
         }
     )
     return {
