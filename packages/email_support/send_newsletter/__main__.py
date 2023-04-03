ebf5b9a2a35ffd8ca9ce282efd13c9c5e9c375aa
houfu <houfu@outlook.stg>
2023-04-03 12:36:37 +0800

    send_newsletter: support plain text email

--- packages/email_support/send_newsletter/__main__.py
+++ packages/email_support/send_newsletter/__main__.py
@@ -8,7 +8,8 @@ address = "sg_law_cookies@mg.your-amicus.app"
 def main(args):
     key = os.getenv('MAILGUN_API_KEY')
     title = args.get('title')
-    content = args.get('content')
+    content_html = args.get('content_html')
+    content_text = args.get('content_text')
     r = requests.post(
         "https://api.mailgun.net/v3/mg.your-amicus.app/messages",
         auth=("api", key),
@@ -16,7 +17,8 @@ def main(args):
             "from": "SG Law Cookies by Your Amicus <cookies@your-amicus.app>",
             "to": address,
             "subject": title,
-            "html": content
+            "html": content_html,
+            "text": content_text
         }
     )
     return {
