862f4dcb5b3fe7092d8b915292c131606ac953de
houfu <houfu@users.noreply.github.com>
2023-11-15 00:43:09 +0800

    Finalise New Cookie Function

--- packages/zeeker_support/new_cookie/__main__.py
+++ packages/zeeker_support/new_cookie/__main__.py
@@ -1,8 +1,40 @@
 import os
-from datetime import datetime
-
+from datetime import date
 
 import requests
+from pydantic import BaseModel, AnyHttpUrl
+
+
+class SGLawCookie(BaseModel):
+    resource_url: AnyHttpUrl
+    cookie_content: str
+    published_date: date
+
 
 def main(args):
-    pass
+    cookie = SGLawCookie.model_validate_json(args["content"])
+    requests.post(
+        f"https://{os.getenv('ZEEKER_URL')}/api/3/action/datastore_create",
+        json={
+            "resource": {
+                "package_id": "sg-law-cookies",
+                "url": cookie.resource_url,
+                "description": f"SG Law Cookies, an algorithmically produced digest of legal news in Singapore, "
+                f"for {cookie.published_date.strftime('%d %B %Y')}",
+            },
+            "fields": [
+                {"id": "date_published", "type": "date"},
+                {"id": "content", "type": "text"},
+            ],
+            "records": [
+                {
+                    "date_published": cookie.published_date.strftime("%d %B %Y"),
+                    "content": cookie.cookie_content,
+                }
+            ],
+        },
+        headers={
+            "Content-Type": "application/json",
+            "Authorization": os.getenv("ZEEKER_API_KEY"),
+        },
+    )
