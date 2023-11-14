c4cf37db1775ea59bd8706673a91da3ca330ce02
houfu <houfu@users.noreply.github.com>
2023-11-14 23:25:19 +0800

    Add zeeker support package

---
+++ packages/zeeker_support/new_cookie/__main__.py
@@ -0,0 +1,8 @@
+import os
+from datetime import datetime
+
+
+import requests
+
+def main(args):
+    pass
---
+++ packages/zeeker_support/new_newsarticle/__main__.py
@@ -0,0 +1,13 @@
+import os
+from datetime import datetime
+
+import requests
+
+
+def main(args):
+    pass
+    # title = args.get('title')
+    # category = args.get('category')
+    # source_link = args.get('source_link')
+    # author = args.get('author')
+    # date =  datetime.strptime(args.get('author'),  "%d %b %Y %H:%M:%S")
