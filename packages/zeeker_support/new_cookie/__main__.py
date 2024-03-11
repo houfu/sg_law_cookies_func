577ef75fe5d3c77867ef7d25655b25b431a89122
houfu <houfu@users.noreply.github.com>
2024-03-11 23:07:02 +0800

    Add datastore

--- packages/zeeker_support/new_cookie/__main__.py
+++ packages/zeeker_support/new_cookie/__main__.py
@@ -30,7 +30,6 @@ def main(args):
         },
     )
     response_json = response.json()
-    resource_response_json = None
     # If resource is created, create a view
     if response_json["success"]:
         resource_response = requests.post(
@@ -46,18 +45,35 @@ def main(args):
             },
         )
         resource_response_json = resource_response.json()
-        print(resource_response_json["success"])
-    else:
-        print(False)
-
-    if resource_response_json:
-        return {
-            "body": {
-                "success": resource_response_json["success"],
-                "error": resource_response_json["error"]
-                if resource_response_json["success"]
-                else None,
-            }
-        }
     else:
         return {"body": {"success": False, "error": response_json["error"]}}
+
+    if resource_response_json["success"]:
+        # Now add to Datastore
+        datastore_response = requests.post(
+            f"https://{os.getenv('ZEEKER_URL')}/api/action/resource_create",
+            json={
+                "resource_id": "e359c6c3-851a-44a2-ad4d-e319642c0098",
+                "method": "insert",
+                "force": "True",
+                "records": [
+                    {
+                        "date_published": cookie.published_date.strftime("%d %B %Y"),
+                        "title": f"SG Law Cookies ({cookie.published_date.strftime('%d %B %Y')})",
+                        "content": cookie.cookie_content,
+                        "resource_url": cookie.resource_url
+                    },
+                ],
+            },
+            headers={
+                "Content-Type": "application/json",
+                "X-CKAN-API-Key": os.getenv("ZEEKER_API_KEY"),
+            },
+        )
+        if datastore_response["success"]:
+            return {"body": {"success": True}}
+        else:
+            return {"body": {"success": False, "error": datastore_response["error"]}}
+
+    else:
+        return {"body": {"success": False, "error": resource_response_json["error"]}}
